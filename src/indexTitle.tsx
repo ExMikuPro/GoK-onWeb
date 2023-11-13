cb275fdc92cb508c3f96cc922de8be5aa457c2e6
ExMikuPro <72144156+ExMikuPro@users.noreply.github.com>
2023-11-13 20:11:11 +0800

    实现导航栏切换改变样式

---
+++ src/indexTitle.tsx
@@ -0,0 +1,10 @@
+const IndexTitle = () => {
+    return (
+        <div class="col-mb-12">
+            <h2 class="title">Hello World</h2>
+            <p class="description">一些简介</p>
+        </div>
+    )
+}
+
+export default IndexTitle
--- src/navBar.tsx
+++ src/navBar.tsx
@@ -1,22 +1,49 @@
+import { createSignal, onCleanup } from "solid-js";
+
+
 const NavBar = () => {
+    const [currentPage, setCurrentPage] = createSignal('index');
+    const [isIndex, setIsIndex] = createSignal(true);
+    const [isLink, setIsLink] = createSignal(false);
+    const [isAbout, setIsAbout] = createSignal(false);
+
+    const changePage = (page: string) => {
+        switch (page) {
+            case 'index':
+                console.log("index");
+                setIsIndex(true);
+                setIsLink(false);
+                setIsAbout(false);
+                return;
+            case 'link':
+                console.log("link");
+                setIsIndex(false);
+                setIsLink(true);
+                setIsAbout(false);
+                return;
+            case 'about':
+                console.log("about");
+                setIsIndex(false);
+                setIsLink(false);
+                setIsAbout(true);
+                return;
+            default:
+                return null;
+        }
+    };
+
     return (
-        <div class="container">
-            <div class="col-mb-12">
-                <h2 class="title">Hello World</h2>
-                <p class="description">一些简介</p>
-            </div>
-            <div class="col-mb-12">
-                <nav>
-                <a>首页</a>
-                <a>友链</a>
-                <a>关于</a>
-                    <div class="search">
-                        <form id="search">
-                            <input type="text" id="s" name="s" class="text" placeholder="/search"></input>
-                        </form>
-                    </div>
-                </nav>
-            </div>
+        <div class="col-mb-12">
+            <nav>
+                <a classList={{ current: isIndex() }} onClick={() => changePage("index")}>首页</a>
+                <a classList={{ current: isLink() }} onClick={() => changePage("link")}>友链</a>
+                <a classList={{ current: isAbout() }} onClick={() => changePage("about")}>关于</a>
+                <div class="search">
+                    <form id="search">
+                        <input type="text" id="s" name="s" class="text" placeholder="/search"></input>
+                    </form>
+                </div>
+            </nav>
         </div>
     )
 }
