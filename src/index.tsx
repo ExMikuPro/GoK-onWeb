33878f3751db6f6e4c633be7e14f8566b22c57bf
ExMikuPro <72144156+ExMikuPro@users.noreply.github.com>
2023-11-13 19:14:18 +0800

    添加网站页面header头部样式

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,62 @@
 import { render } from 'solid-js/web';
 
 import './index.css';
+import './navBar.css';
 import App from './App';
 import NarBar from './navBar'
 
-render(() => <NarBar />,document.getElementById('header')!);
-render(() => <App />,document.getElementById('row')!);
+// render(() => <NarBar />,document.getElementById('header')!);
+
+render(() => <NarBar /> , document.getElementById("header")!)
+render(() => <App />, document.getElementById('row')!);
+
+
+/*
+
+import { createSignal, onCleanup } from 'solid-js';
+
+const NavBar = () => {
+  const [currentPage, setCurrentPage] = createSignal('home');
+
+  const changePage = (page:string) => {
+    setCurrentPage(page);
+  };
+
+  const renderPage = () => {
+    switch (currentPage()) {
+      case 'home':
+        return <div>这是首页内容</div>;
+      case 'about':
+        return <div>这是关于页面内容</div>;
+      case 'contact':
+        return <div>这是联系页面内容</div>;
+      default:
+        return null;
+    }
+  };
+
+  onCleanup(() => {
+  });
+
+  return (
+    <div>
+        <div class="col-mb-12">
+                <h2 class="title">标题</h2>
+                <p class="description">123</p>
+            </div>
+            <div class="col-mb-12"></div>
+      <nav>
+      <a class='current' onClick={() => changePage("home")}>首页</a>
+      <a class='current' onClick={() => changePage("home")}>首页</a>
+      <a class='current' onClick={() => changePage("about")}>首页</a>
+      <a class='current' onClick={() => changePage("contact")}>首页</a>
+      </nav>
+      {renderPage()}
+    </div>
+  );
+};
+
+export default NavBar;
+
+
+*/
--- src/navBar.tsx
+++ src/navBar.tsx
@@ -1,44 +1,24 @@
-import { createSignal, onCleanup } from 'solid-js';
-
 const NavBar = () => {
-  const [currentPage, setCurrentPage] = createSignal('home');
-
-  const changePage = (page:string) => {
-    setCurrentPage(page);
-  };
-
-  const renderPage = () => {
-    switch (currentPage()) {
-      case 'home':
-        return <div>这是首页内容</div>;
-      case 'about':
-        return <div>这是关于页面内容</div>;
-      case 'contact':
-        return <div>这是联系页面内容</div>;
-      default:
-        return null;
-    }
-  };
-
-  onCleanup(() => {
-  });
-
-  return (
-    <div>
-        <div class="col-mb-12">
-                <h2 class="title">标题</h2>
-        	    <p class="description">123</p>
+    return (
+        <div class="container">
+            <div class="col-mb-12">
+                <h2 class="title">Hello World</h2>
+                <p class="description">一些简介</p>
             </div>
-            <div class="col-mb-12"></div>
-      <nav>
-      <a class='current' onClick={() => changePage("home")}>首页</a>
-      <a class='current' onClick={() => changePage("home")}>首页</a>
-      <a class='current' onClick={() => changePage("about")}>首页</a>
-      <a class='current' onClick={() => changePage("contact")}>首页</a>
-      </nav>
-      {renderPage()}
-    </div>
-  );
-};
+            <div class="col-mb-12">
+                <nav>
+                <a>首页</a>
+                <a>友链</a>
+                <a>关于</a>
+                    <div class="search">
+                        <form id="search">
+                            <input type="text" id="s" name="s" class="text" placeholder="/search"></input>
+                        </form>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    )
+}
 
 export default NavBar;
